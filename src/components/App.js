0655401fb0a489587f72ac93e850873a50aecf9b
James <jamesrobertsongroves@gmail.com>
2022-11-28 17:21:02 +0000

    default to music

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import './App.css';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('music'); //default loading music
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -14,7 +19,10 @@ class App extends React.Component {
       },
     });
     //console.log(response.data.items)
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0] //adding default video
+    });
   };
 
   onVideoSelect = (video) => {
@@ -26,16 +34,16 @@ class App extends React.Component {
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
-        <div className='ui row'>
-        <div className='eleven wide column'>
-          <VideoDetail video={this.state.selectedVideo} />
-          </div>
-          <div className='five wide column'>
-          <VideoList
-            onVideoSelect={this.onVideoSelect}
-            videos={this.state.videos}
-          />
-          </div>
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
           </div>
         </div>
       </div>
